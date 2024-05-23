9833189077844c2c0067de575d36581e429585a3
sailendrachettri <sailendra9083@gmail.com>
2024-05-24 03:18:23 +0530

    client is now connected with server

--- routes/auth.js
+++ routes/auth.js
@@ -37,9 +37,8 @@ router.post('/signup', async (req, res) => {
 
             // jwt token generation
             const data = {
-                user: {
-                    id: user.id
-                }
+                    id: user.id,
+                    fullname,
             }
             const auth_token = jwt.sign(data, process.env.JWT_SECRET_KEY);
 
@@ -92,6 +91,39 @@ router.post('/login', async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ success, message: "Internal server errrrror", error: error.message });
+    }
+})
+
+// ROUTE 3: Get all the user information (LOGGED IN)
+router.get('/profile', async(req, res) => {
+    let success = false;
+    const { auth_token } = req.cookies;
+
+    try {
+        success = true;
+        const userInfo = jwt.verify(auth_token, process.env.JWT_SECRET_KEY);
+
+        // get the user detials expect password
+        // const user = await Users.findById(userInfo.id).select("-password");
+
+        // res.status(200).json({success: true, userInfo, user: user});
+        res.status(200).json(userInfo);
+
+    } catch (err) {
+        success = false;
+        res.status(500).json({ success, message: "Internal server errorrr", error: err.message })
+    }
+})
+
+// ROUTE 4: Handle logout
+router.post('/logout', (_, res) => { // if the req is not required the we can use underscore
+    try {
+        res.clearCookie('auth_token', {
+            sameSite: 'none',
+            secure: true
+        }).json({success: true, message: "Auth Token replaced with empty string"}); // removing auth_token from cookie
+    } catch (err) {
+        res.status(500).json({meccess: false, message: err.message});
     }
 })
 
